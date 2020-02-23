2aa970731d571c5b4810672dcd60ea181fcded47
James Cai <jamescai@genomezoo.net>
2020-02-22 23:04:09 -0600

    Update scTenifoldNet.jl

--- src/scTenifoldNet.jl
+++ src/scTenifoldNet.jl
@@ -19,4 +19,46 @@ function pcnet(X)
     return A
 end
 
+function tensordecomp(X)
+    a=TensorToolbox.cp_als(X,5)
+    Z=full(a)
+    Z1=mean(Z[:,:,i] for i=1:size(Z,3))
+    Z1=Z1./maximum(abs.(Z1));
+    # Z1=round.(Z1; digits=5)
+    return Z1
+end
+
+function manialn(X,Y)
+    μ,dim=0.9,30
+    n1,n2=size(X,1),size(Y,1);
+    W₁,W₂=X.+1,Y.+1
+    ℐ=Matrix(I,n1,n2);
+    μ = μ*(sum(W₁)+sum(W₂)/(2*sum(ℐ)));
+    𝕎 = [W₁ μ*ℐ; μ*ℐ' W₂];
+    L=diagm(vec(sum(abs.(𝕎),dims=1))).-𝕎;
+    λ,V = eigen(L);
+    i=real(λ).>=1e-8;
+    V=real(V[:,i]);
+    V=V[:,1:dim];    
+    aln1=V[1:n1,:];
+    aln2=V[n1+1:end,:];
+    dd = norm.((aln1.-aln2)[i,:] for i = 1:n1)
+    # _,idx=findmax(dd)
+    return sortperm(-dd)    
+end
+
+function dddtenifold(X)
+    lbsz=sum(X,dims=1)
+    X=(X./lbsz)*median(lbsz)
+    ℊ,𝒸=size(X)
+    A=zeros(Float64, ℊ, ℊ, 10)
+    for k=1:10
+        println("network ... $k")
+        𝕩=X[:,randperm(𝒸)][:,1:500]
+        A[:,:,k]=pcnetworkhelp(𝕩')
+    end
+    Z=tensordecomp(A)
+    return Z
+end
+
 end # module
